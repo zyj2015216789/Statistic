0caa004a63e66c659ed0527758b2a53f55e34493
zyj2015216789 <1957487135@qq.com>
2022-04-12 21:48:08 +0800

    Update ReactiveForwardingCommand.java

--- ifwd/src/main/java/org/onosproject/ifwd/ReactiveForwardingCommand.java
+++ ifwd/src/main/java/org/onosproject/ifwd/ReactiveForwardingCommand.java
@@ -44,3 +44,38 @@ public class ReactiveForwardingCommand extends AbstractShellCommand {
         reactiveForwardingService.printMetric(macAddress);
     }
 }
+public class QpsRateLimiter {
+
+    private long lastTime;
+
+    private long interval;
+
+    private AtomicInteger remaind;
+
+    private int rate;
+
+    public QpsRateLimiter(int rate, long interval, TimeUnit unit) {
+        this.rate = rate;
+        this.interval = unit.toMillis(interval);
+        this.lastTime = System.currentTimeMillis();
+        this.remaind = new AtomicInteger(rate);
+    }
+
+    public boolean allowable() {
+        long now = System.currentTimeMillis();
+        if (now > lastTime + interval) {
+            remaind.set(rate);
+            lastTime = now;
+        }
+
+        int value = remaind.get();
+        boolean flag = false;
+        while (value > 0 && !flag) {
+            flag = remaind.compareAndSet(value, value - 1);
+            value = remaind.get();
+        }
+
+        return flag;
+    }
+
+}
